9ca81e8a22fe4e12c6262dfce8e519d1506b45c2
Steve Souza <steve@stevesouza.com>
2021-01-16 03:59:14 -0500

    added code to test converting from a google sheet to a pandas dataframe

--- googlesheets/mytester.py
+++ googlesheets/mytester.py
@@ -5,7 +5,22 @@ import googlesheets2 as gs
 if __name__ == '__main__':
     creds_file = "/Users/stevesouza/.kettle/client_secret.json"
     id = "18VF0mB6usVkorgCULDqd4Ib9UrWf8MnaqYxrtnYcsvo"
-    sheet = "Sheet1"  # A1 notation
-    google_sheet = gs.GoogleSheet(creds_file)
-    data = google_sheet.get_data(id, sheet)
+    sheet = "Sheet1" # A1 notation
+    spreadsheet = gs.GoogleSheet(creds_file)
+    data = spreadsheet.get_data(id, sheet)
+    print("** spreadsheet data")
     print(data)
+    print(f"length: {len(data)}")
+
+    # The first row of this spreadsheet contains the header
+    # Note this is also required to remove the first row from the data if the header was included
+    header = data.pop(0)
+    print(f"\n** header: {header}\n")
+
+    df = utils.to_pandas(data, header)
+    print("** dataframe")
+    print(df)
+
+    print("\n** dataframe,dtypes")
+    print(df.dtypes)
+
