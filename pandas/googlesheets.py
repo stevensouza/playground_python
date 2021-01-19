bb5d866f0355c381eb913113c72978e32d14f2a9
Steve Souza <steve@stevesouza.com>
2021-01-19 15:38:01 -0500

    improved comment

--- pandas/googlesheets.py
+++ pandas/googlesheets.py
@@ -32,8 +32,8 @@ class GoogleSheet:
 
     def get_data(self, google_sheet_id, sheet_a1_notation):
         """
-            Returns dictionary where each row of the spreadsheet is an array.  The first row could contain
-            the header if the spreadsheet is set up that way.
+            Returns a list of lists where each row of the spreadsheet is a list.  The first row/list could contain
+            the header if the spreadsheet is set up that way. Example: [["name","age"],["joe",82],["bo",33]]
         """
         # use Google Sheets API to pull the spreadsheet data/rows
         # https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets/get?apix_params=%7B%22spreadsheetId%22%3A%2218VF0mB6usVkorgCULDqd4Ib9UrWf8MnaqYxrtnYcsvo%22%2C%22includeGridData%22%3Atrue%2C%22ranges%22%3A%5B%22Sheet1%22%5D%7D
@@ -54,8 +54,8 @@ class GoogleSheet:
         :param overwrite_or_append: OVERWRITE or APPEND rows
         :param google_sheet_id:
         :param sheet_a1_notation: The sheet name
-        :param rows: rows as a list of lists [['joe','smith'],['joe','jones']]. Note a header can be included as
-            the first row.
+        :param rows: rows as a list of lists [['joe','smith'],['joe','jones']]. Note a header can optionally
+            be included as the first row.
         :param value_input_option: How to format the cells. See google documentation
         :return: The results from the Google API call
         """
